5f4b98bd9d03d8a231ff3432c9386692ac6d9737
hjkim0905 <eax9952@gmail.com>
2024-04-12 22:03:08 +0900

    240412 : DownloadController 구현 완료 및 과제 최종 완성

--- 3rd_hw_KimHyunJoong/3rd_hw_KimHyunJoong/DownloadController.swift
+++ 3rd_hw_KimHyunJoong/3rd_hw_KimHyunJoong/DownloadController.swift
@@ -19,24 +19,26 @@ class DownloadController: UIViewController{
         let label1: UILabel = {
             let label = UILabel()
             label.translatesAutoresizingMaskIntoConstraints = false
-            label.text = "HEART"
-            label.textColor = .white
+            label.text = "Movies and TV shows that you"
+            label.textColor = .lightGray
             label.font = UIFont.systemFont(ofSize: 50, weight: .bold)
             return label
         }()
-//        let image: UIImageView = {
-//            let image1 = UIImageView()
-//            // systemImage의 크기를 조정하기 위해서는 symbolConfiguration을 지정해야 한다.
-//            let imageSize = UIImage.SymbolConfiguration(pointSize: 50, weight: .light)
-//            image1.translatesAutoresizingMaskIntoConstraints = false
-//            image1.image = UIImage(systemName: "heart.fill", withConfiguration: imageSize)
-//            image1.tintColor = .white
-//            image1.contentMode = .scaleAspectFit
-//            return image1
-//        }()
-//
-//        view.addSubview(image)
-        view.addSubview(label1)
+        
+        let label2: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "download appear here."
+            label.textColor = .lightGray
+            label.font = UIFont.systemFont(ofSize: 50, weight: .bold)
+            return label
+        }()
+        
+        let image: UIImage = {
+            
+        }
+
+        view.addSubview(label2)
         
         
         NSLayoutConstraint.activate([
--- 4th_hw_KimHyunJoong/3rd_hw_KimHyunJoong/View/DownloadController.swift
+++ 4th_hw_KimHyunJoong/3rd_hw_KimHyunJoong/View/DownloadController.swift
@@ -1,49 +1,77 @@
-//
-//  DownloadController.swift
-//  TabBar
-//
-//  Created by 김하람 on 2023/09/29.
-//
-
 import UIKit
 
-
-class DownloadController: UIViewController{
+class DownloadController: UIViewController {
+    
+    let downloadImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "download"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let messageLabelView1: UILabel = {
+        let label = UILabel()
+        label.text = "Movies and TV shows that you."
+        label.textColor = .lightGray
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let messageLabelView2: UILabel = {
+        let label = UILabel()
+        label.text = "download appear here."
+        label.textColor = .lightGray
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let downloadButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Find Something to Download", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
-        setUI()
+        
+        view.backgroundColor = .black
+        
+        view.addSubview(downloadImageView)
+        view.addSubview(messageLabelView1)
+        view.addSubview(messageLabelView2)
+        view.addSubview(downloadButton)
+        
+        setupConstraints()
     }
     
-    func setUI(){
-        let label1: UILabel = {
-            let label = UILabel()
-            label.translatesAutoresizingMaskIntoConstraints = false
-            label.text = "HEART"
-            label.textColor = .white
-            label.font = UIFont.systemFont(ofSize: 50, weight: .bold)
-            return label
-        }()
-//        let image: UIImageView = {
-//            let image1 = UIImageView()
-//            // systemImage의 크기를 조정하기 위해서는 symbolConfiguration을 지정해야 한다.
-//            let imageSize = UIImage.SymbolConfiguration(pointSize: 50, weight: .light)
-//            image1.translatesAutoresizingMaskIntoConstraints = false
-//            image1.image = UIImage(systemName: "heart.fill", withConfiguration: imageSize)
-//            image1.tintColor = .white
-//            image1.contentMode = .scaleAspectFit
-//            return image1
-//        }()
-//
-//        view.addSubview(image)
-        view.addSubview(label1)
-        
-        
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            label1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label1.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            downloadImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            downloadImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
+            downloadImageView.widthAnchor.constraint(equalToConstant: 200),
+            downloadImageView.heightAnchor.constraint(equalToConstant: 200),
+            
+            messageLabelView1.topAnchor.constraint(equalTo: downloadImageView.bottomAnchor, constant: 20),
+            messageLabelView1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            messageLabelView1.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            messageLabelView2.topAnchor.constraint(equalTo: messageLabelView1.bottomAnchor, constant: 5),
+            messageLabelView2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            messageLabelView2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            downloadButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            downloadButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            downloadButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            downloadButton.heightAnchor.constraint(equalToConstant: 50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
-
-
