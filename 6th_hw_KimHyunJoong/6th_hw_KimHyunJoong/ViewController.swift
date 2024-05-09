b0d01ca750bfbce2458739ef407e128f71a2f91a
hjkim0905 <eax9952@gmail.com>
2024-05-09 23:27:10 +0900

    240509 : 6차 과제 프로젝트 재생성

---
+++ 6th_hw_KimHyunJoong/6th_hw_KimHyunJoong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6th_hw_KimHyunJoong
+//
+//  Created by 김현중 on 5/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
